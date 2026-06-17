fb27d3d4f84a0d283fc2096c7bdc427b5b78a581
ngoyimatthieu03-coder <ngoyimatthieu.03@gmail.com>
2026-06-17 10:50:03 +0100

    commit 3  : dans la branch-2  modification  du commentaire

--- guess-number.py
+++ guess-number.py
@@ -5,7 +5,7 @@ Max_tentative=5
 print("Guest the Number")
 print("devine le nombre entre 1 et 100 ")
 
- #ceci est une boucle
+ #ceci est une boucle et son debut
 while tentative < Max_tentative:
     n=int(input("Monsieur - medame Entrez votre proposition:"))
     tentative +=1
