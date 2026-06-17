bb287f4d4dc39dbb8b161cd34dfbd05ebf2b4170
ngoyimatthieu03-coder <ngoyimatthieu.03@gmail.com>
2026-06-17 10:45:09 +0100

    commit  1 : dans la branch 2 correction du code

---
+++ guess-number.py
@@ -0,0 +1,22 @@
+import random 
+nombresecret = random.randint(1,100)
+tentative=0
+Max_tentative=5
+print("Guest the Number")
+print("devine le nombre entre 1 et 100 ")
+
+ #ceci est une boucle
+while tentative < Max_tentative:
+    n=int(input("Monsieur - medame Entrez votre proposition:"))
+    tentative +=1
+    if n < nombresecret:
+        print ("Le nombre est trop petit")
+        # Je fait des commit
+    elif n > nombresecret:
+        print("Le nombre est trop grang est")
+    else:
+        print("Monsieur ou Medame Merci! vous avez trouvez le le nombre")
+        break
+        print("Game Over!")
+        print(f"Le nombre est était{nombresecret}")
+        # ceci est la fin du cboucle while
